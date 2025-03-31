4358482f22ee20b77e8138e3b2c0429aa4d6e45d
AMEYSATI <78717195+AMEYSATI@users.noreply.github.com>
2025-03-31 13:09:12 +0530

    Fixed bcrypt ELF header issue

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import pool from "../db.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
